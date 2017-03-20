a00e52add3f5ee39f1548ce5e31684a5eed3ae13
Farhan Wazir <farhan.wazir@gmail.com>
2017-03-20 17:14:29 +0500

    Modified sys function as core.

--- Functions/General.php
+++ Functions/General.php
@@ -4,8 +4,22 @@ function checkActiveURL($uri){
     return ('http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']) == $uri ? 'active' : '';
 }
 
-function sys($key){
-    return app($key);
+if (!function_exists('sys')) {
+    /**
+     * Get the available container instance.
+     *
+     * @param  string $make
+     * @param  array $parameters
+     * @return mixed|\Illuminate\Contracts\Foundation\Application
+     */
+    function sys($make = null, $parameters = [])
+    {
+        if (is_null($make)) {
+            return app('Illuminate\Container\Container');
+        }
+
+        return app('Illuminate\Container\Container')->make($make, $parameters);
+    }
 }
 
 
